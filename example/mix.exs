03b16b2aeeef1006f0208c59c0339e6f33676b94
Samuel Ventura <samuel.ventura@yeico.com>
2022-06-29 05:08:04 -0500

    qt5webengine fails to compile

---
+++ example/mix.exs
@@ -0,0 +1,63 @@
+defmodule Example.MixProject do
+  use Mix.Project
+
+  @app :qtwebeng
+  @version "0.1.0"
+  @all_targets [:x86_64]
+
+  def project do
+    [
+      app: @app,
+      version: @version,
+      elixir: "~> 1.9",
+      archives: [nerves_bootstrap: "~> 1.10"],
+      start_permanent: Mix.env() == :prod,
+      build_embedded: true,
+      deps: deps(),
+      releases: [{@app, release()}],
+      preferred_cli_target: [run: :host, test: :host]
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      mod: {Example.Application, []},
+      extra_applications: [:logger, :runtime_tools]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      # Dependencies for all targets
+      {:nerves, "~> 1.7.15", runtime: false},
+      {:shoehorn, "~> 0.8.0"},
+      {:ring_logger, "~> 0.8.3"},
+      {:toolshed, "~> 0.2.13"},
+
+      # Dependencies for all targets except :host
+      {:nerves_runtime, "~> 0.11.6", targets: @all_targets},
+      {:nerves_pack, "~> 0.6.0", targets: @all_targets},
+
+      # Dependencies for specific targets
+      # NOTE: It's generally low risk and recommended to follow minor version
+      # bumps to Nerves systems. Since these include Linux kernel and Erlang
+      # version updates, please review their release notes in case
+      # changes to your application are needed.
+      {:nerves_system_x86_64, path: "../", runtime: false, targets: :x86_64}
+    ]
+  end
+
+  def release do
+    [
+      overwrite: true,
+      # Erlang distribution is not started automatically.
+      # See https://hexdocs.pm/nerves_pack/readme.html#erlang-distribution
+      cookie: "#{@app}_cookie",
+      include_erts: &Nerves.Release.erts/0,
+      steps: [&Nerves.Release.init/1, :assemble],
+      strip_beams: Mix.env() == :prod or [keep: ["Docs"]]
+    ]
+  end
+end
---
+++ example/priv/rect.qml
@@ -0,0 +1,8 @@
+import QtQuick 2.0
+
+Rectangle {
+    id: content
+    width: 800
+    height: 600
+    color: "green"
+}
